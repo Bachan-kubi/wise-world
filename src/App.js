452f6eaf4f5315ac64dcd759b87392dfbc7d9d1e
bachan kubi <felixghagra@gmail.com>
2022-10-11 18:48:17 +0600

    bar chart added

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ function App() {
         },
         {
           path: 'statistics',
-          loader: ()=>{
-
-          },
+          loader: ()=>fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistics />
         },
         {
--- src/Statistics/Statistics.js
+++ src/Statistics/Statistics.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import './Statistics.css';
 
 const Statistics = () => {
+    const QuizData = useLoaderData()
     return (
-        <div>
-            <h4>This is Statictics</h4>
+        <div className='chart'>
+            <div>
+                <ResponsiveContainer width='92%' height={400}>
+                    <BarChart data={QuizData.data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#82ca9d" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
