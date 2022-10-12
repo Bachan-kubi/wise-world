e3f7ba03e7d8c8c61a5a55e3e5864e9081897210
bachan kubi <felixghagra@gmail.com>
2022-10-12 12:59:59 +0600

    header section added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Books from "../Books/Books";
-import './Home.css'
+import "./Home.css";
+import Image from "../img/quiz-blue-banner-3d-style-white_123447-766.jpg";
 
 const Home = () => {
   const allBooks = useLoaderData();
   return (
     <div>
-      <h3>this is home - need details. import another component here.</h3>
+      <div className="home-container">
+        <img src={Image} alt="banner" />
+        <h2>Test your Skills, Lift yourself Higher.</h2>
+      </div>
       <div className="home">
         {allBooks.data.map((book) => (
           <Books key={book.id} book={book} />
